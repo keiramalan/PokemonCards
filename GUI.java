26a29f17c056ed0226e87f58d9e064d437f9d070
Keira Malan <keira.malan@student.onslow.school.nz>
2021-07-01 12:04:49 +1200

    Finished hopefully

--- GUI.java
+++ GUI.java
@@ -30,7 +30,17 @@ public class GUI
     final int MAXNAME = 20;
     final int MINNAME = 1;
     
+    // checker for validity of user's inputted card price
+    boolean priceValid;
+    
+    // stores current card to display
     Card currCard;
+    
+    // Positions of text when displaying a card
+    final int XPOS = 100;
+    int yPos;
+    // allows text blocks to not stack on top of each other
+    final int YSPACER = 30;
 
     /**
      * Constructor for objects of class GUI
@@ -54,21 +64,52 @@ public class GUI
     public void addCard() {
         // amount to increment card ID for hashmap placement
         final int INCREMENT = 1;
+        priceValid = false;
         
         // Get card's name and number from user
-        String name = "Name";
+        String name;
         int cardPrice = -1;
         
         // Name must be larger than 0 characters but smaller than 20
         do {
-            name = UI.askString("Name: ");
+            name = UI.askString("Card Name (1-20 chars): ");
+            // Remove any uneccessary whitespace
+            name = name.strip();
         } while (name.length() < MINNAME || name.length() > MAXNAME);
         
-        // price cannot be higher than $300,000 or lower than 1
-        do {
-            cardPrice = UI.askInt("Card price: ");
-        } while (cardPrice < MINPRICE || cardPrice > MAXPRICE || cardPrice != -1);
-        
+        // Loop for valid card price
+        while (priceValid == false) {
+            // ask for price as string to check for null values
+            String priceString = UI.askString("Card Price: ");
+            // Strip any whitespace
+            priceString = priceString.strip();
+            
+            // check user has entered a value
+            if (priceString != null) {
+
+                // STRING TO INT CODE SOURCED FROM
+                //https://stackoverflow.com/questions/5585779/
+                //how-do-i-convert-a-string-to-an-int-in-java
+                
+                try {
+                    cardPrice = Integer.parseInt(priceString);
+                    // Check card price is a reasonable number
+                    if (cardPrice >= MINPRICE || cardPrice <= MAXPRICE) {
+                        // End loop
+                        priceValid = true;
+                    }
+                    else {
+                        UI.println("Please enter a number between 1 and 3000000!");
+                    }
+                }
+                catch (NumberFormatException e) {
+                    // Error message if user doesn't enter an integer
+                    UI.println("Please enter an integer!");
+                }
+            }
+            
+        }
+
         // add an image of the card
         String imgFileName = UIFileChooser.open("Choose Image File: ");
         
@@ -88,11 +129,7 @@ public class GUI
         UI.clearGraphics();
         
         // co-ordinates of text
-        final int XPOS = 100;
-        int yPos = 400;
-        
-        // allows text blocks to not stack on top of each other
-        int ySpacer = 30;
+        yPos = 400;
         
         UI.setColor(Color.black);
         
@@ -100,12 +137,12 @@ public class GUI
         UI.drawString(selectedCard.getName(), XPOS, yPos); 
         
         // convert price from int to string to print
-        // CODE SOURCED FROM 
+        /* CODE SOURCED FROM */
         // https://www.javatpoint.com/java-double-to-string
         String cardPrice = String.valueOf(selectedCard.getValue());
         
         // Print price of card
-        UI.drawString("Price: $" + cardPrice, XPOS, yPos+ySpacer);
+        UI.drawString("Price: $" + cardPrice, XPOS, yPos+YSPACER);
         
         // Print card image on canvas
         displayImage(selectedCard.getImage());
@@ -126,11 +163,16 @@ public class GUI
      */
     public void findCard() {
         String cardName;
+        // Get user's inputted card
         do {
-            cardName = UI.askString("Name of card: ");
-        } while (cardName.length() < MINNAME || 
+            cardName = UI.askString
+            ("Name of card: (1-20 characters) ");
+            // Remove any whitespace
+            cardName = cardName.strip();
+        } while (cardName.length() < MINNAME || // Check input meets name length requirements
         cardName.length() > MAXNAME); 
         
+        // Search for card in hashmap
         if (crds.findCard(cardName)) {
             UI.println("Found this card!");
             // retrieve card from hashmap to print
@@ -169,7 +211,7 @@ public class GUI
         
         // Loop through hashmap to display each card's details
         for (int cardId = 1; cardId <= mapLength; cardId++) {
-            // locate current card
+            // retrive card to display
             currCard = crds.getCard(cardId);
             // Print name
             UI.println("Name of card: " + currCard.getName());
